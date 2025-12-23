23754f437eb548737bd0ee00780c020fb1a1ebe0
akash-1191 <158453744+akash-1191@users.noreply.github.com>
2025-12-23 23:44:20 +0530

    cc

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  SECRET_KEY_Razorpay: "rzp_test_PQ2r2RUKU4ACiT",
+  apiBaseUrl: 'https://aspcoreezride.onrender.com/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  SECRET_KEY_Razorpay: "rzp_test_PQ2r2RUKU4ACiT",
+  apiBaseUrl: 'https://aspcoreezride.onrender.com/api'
+};
